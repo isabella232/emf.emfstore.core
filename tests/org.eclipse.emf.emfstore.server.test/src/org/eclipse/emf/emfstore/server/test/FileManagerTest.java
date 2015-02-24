0c1ec87e02d59cbfd296a4124c865620085ff941
Edgar Mueller <emueller@eclipsesource.com>
2015-02-24 10:04:49 +0100

    Bug 460275 - Support lazy loading of local change package
    Testing - made FileManagerTest self-contained

--- tests/org.eclipse.emf.emfstore.server.test/src/org/eclipse/emf/emfstore/server/test/FileManagerTest.java
+++ tests/org.eclipse.emf.emfstore.server.test/src/org/eclipse/emf/emfstore/server/test/FileManagerTest.java
@@ -5,7 +5,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * mkoegel
  ******************************************************************************/
@@ -13,6 +13,7 @@ package org.eclipse.emf.emfstore.server.test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -20,25 +21,45 @@ import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.eclipse.emf.emfstore.client.exceptions.ESServerStartFailedException;
 import org.eclipse.emf.emfstore.client.test.common.dsl.Add;
 import org.eclipse.emf.emfstore.client.test.common.dsl.Create;
 import org.eclipse.emf.emfstore.client.test.common.util.ProjectUtil;
+import org.eclipse.emf.emfstore.client.test.common.util.ServerUtil;
 import org.eclipse.emf.emfstore.internal.client.model.filetransfer.FileDownloadStatus;
 import org.eclipse.emf.emfstore.internal.client.model.filetransfer.FileDownloadStatus.Status;
 import org.eclipse.emf.emfstore.internal.server.model.FileIdentifier;
 import org.eclipse.emf.emfstore.server.exceptions.ESException;
 import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
  * Tests the file manager.
- * 
+ *
  * @author mkoegel
- * 
+ *
  */
 public class FileManagerTest extends TransmissionTests {
 
 	private File file;
+
+	@BeforeClass
+	public static void beforeClass() {
+		try {
+			ServerUtil.startServer();
+		} catch (final IllegalArgumentException ex) {
+			fail(ex.getMessage());
+		} catch (final ESServerStartFailedException ex) {
+			fail(ex.getMessage());
+		}
+	}
+
+	@AfterClass
+	public static void afterClass() {
+		ServerUtil.stopServer();
+	}
 
 	@After
 	@Override
--- tests/org.eclipse.emf.emfstore.server.test/src/org/eclipse/emf/emfstore/server/test/TransmissionTests.java
+++ tests/org.eclipse.emf.emfstore.server.test/src/org/eclipse/emf/emfstore/server/test/TransmissionTests.java
@@ -5,7 +5,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  ******************************************************************************/
 package org.eclipse.emf.emfstore.server.test;
@@ -13,15 +13,11 @@ package org.eclipse.emf.emfstore.server.test;
 import static org.eclipse.emf.emfstore.client.test.common.util.ProjectUtil.nullProgressMonitor;
 import static org.junit.Assert.fail;
 
-import org.eclipse.emf.emfstore.client.test.common.builders.BOOL.TRUE;
-import org.eclipse.emf.emfstore.client.test.common.builders.UserBuilder;
 import org.eclipse.emf.emfstore.client.test.common.cases.ESTestWithLoggedInUser;
-import org.eclipse.emf.emfstore.client.test.common.dsl.Create;
 import org.eclipse.emf.emfstore.client.test.common.dsl.Delete;
 import org.eclipse.emf.emfstore.internal.client.model.ProjectSpace;
 import org.eclipse.emf.emfstore.internal.client.model.Usersession;
 import org.eclipse.emf.emfstore.internal.client.model.impl.api.ESLocalProjectImpl;
-import org.eclipse.emf.emfstore.internal.client.model.impl.api.ESUsersessionImpl;
 import org.eclipse.emf.emfstore.internal.server.model.accesscontrol.ACOrgUnitId;
 import org.eclipse.emf.emfstore.server.exceptions.ESException;
 import org.junit.After;
@@ -68,32 +64,32 @@ public abstract class TransmissionTests extends ESTestWithLoggedInUser {
 			fail(ex.getMessage());
 		}
 
-		final UserBuilder<TRUE, TRUE, TRUE> builder1 = UserBuilder.create()
-			.withName(WRITER1)
-			.withPassword(FOO)
-			.onServer(getServer());
-		final UserBuilder<TRUE, TRUE, TRUE> builder2 = UserBuilder.create()
-			.withName(WRITER2)
-			.withPassword(BAR)
-			.onServer(getServer());
-
-		try {
-			user1 = Create.user(builder1);
-			user2 = Create.user(builder2);
-		} catch (final ESException ex) {
-			fail(ex.getMessage());
-		}
-
-		try {
-			usersession1 = ESUsersessionImpl.class.cast(getServer().login(
-				builder1.getUsername(),
-				builder1.getPassword())).toInternalAPI();
-			usersession2 = ESUsersessionImpl.class.cast(getServer().login(
-				builder2.getUsername(),
-				builder2.getPassword())).toInternalAPI();
-		} catch (final ESException ex) {
-			fail(ex.getMessage());
-		}
+		// final UserBuilder<TRUE, TRUE, TRUE> builder1 = UserBuilder.create()
+		// .withName(WRITER1)
+		// .withPassword(FOO)
+		// .onServer(getServer());
+		// final UserBuilder<TRUE, TRUE, TRUE> builder2 = UserBuilder.create()
+		// .withName(WRITER2)
+		// .withPassword(BAR)
+		// .onServer(getServer());
+		//
+		// try {
+		// user1 = Create.user(builder1);
+		// user2 = Create.user(builder2);
+		// } catch (final ESException ex) {
+		// fail(ex.getMessage());
+		// }
+		//
+		// try {
+		// usersession1 = ESUsersessionImpl.class.cast(getServer().login(
+		// builder1.getUsername(),
+		// builder1.getPassword())).toInternalAPI();
+		// usersession2 = ESUsersessionImpl.class.cast(getServer().login(
+		// builder2.getUsername(),
+		// builder2.getPassword())).toInternalAPI();
+		// } catch (final ESException ex) {
+		// fail(ex.getMessage());
+		// }
 	}
 
 	@Override
