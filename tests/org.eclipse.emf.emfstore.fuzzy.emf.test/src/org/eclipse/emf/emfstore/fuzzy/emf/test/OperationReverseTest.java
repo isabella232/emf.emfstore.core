2bd8120e86bd1076bf845044e110214a054c28d2
Edgar Mueller <emueller@eclipsesource.com>
2015-02-25 15:35:00 +0100

    Bug 460275 - Support lazy loading of local change package
    Fixed compile error

--- tests/org.eclipse.emf.emfstore.fuzzy.emf.test/src/org/eclipse/emf/emfstore/fuzzy/emf/test/OperationReverseTest.java
+++ tests/org.eclipse.emf.emfstore.fuzzy.emf.test/src/org/eclipse/emf/emfstore/fuzzy/emf/test/OperationReverseTest.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
  * Copyright (c) 2012-2013 EclipseSource Muenchen GmbH and others.
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * Julian Sommerfeldt - initial API and implementation
  * Edgar Mueller - bug fixing
@@ -18,15 +18,16 @@ import org.eclipse.emf.emfstore.fuzzy.emf.junit.ESFuzzyRunner;
 import org.eclipse.emf.emfstore.internal.client.model.impl.ProjectSpaceBase;
 import org.eclipse.emf.emfstore.internal.client.model.util.EMFStoreCommand;
 import org.eclipse.emf.emfstore.internal.server.model.versioning.operations.AbstractOperation;
+import org.eclipse.emf.emfstore.internal.server.model.versioning.persistent.CloseableIterable;
 import org.eclipse.emf.emfstore.modelmutator.ESModelMutatorConfiguration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * Fuzzy ESFuzzyTest for the reverse functionality of operations.
- * 
+ *
  * @author Julian Sommerfeldt
- * 
+ *
  */
 @RunWith(ESFuzzyRunner.class)
 @DataProvider(ESEMFDataProvider.class)
@@ -46,13 +47,19 @@ public class OperationReverseTest extends FuzzyProjectTest {
 			}
 		}.run(false);
 
-		final Iterable<AbstractOperation> reversedOperations = projectSpace.changePackage().reversedOperations();
-		new EMFStoreCommand() {
-			@Override
-			protected void doRun() {
-				projectSpace.applyOperations(reversedOperations, false);
-			}
-		}.run(false);
+		final CloseableIterable<AbstractOperation> reversedOperations = projectSpace.changePackage()
+			.reversedOperations();
+
+		try {
+			new EMFStoreCommand() {
+				@Override
+				protected void doRun() {
+					projectSpace.applyOperations(reversedOperations.iterable(), false);
+				}
+			}.run(false);
+		} finally {
+			reversedOperations.close();
+		}
 
 		compareIgnoreOrder(projectSpace.getProject(), getCopyProjectSpace().getProject());
 	}
