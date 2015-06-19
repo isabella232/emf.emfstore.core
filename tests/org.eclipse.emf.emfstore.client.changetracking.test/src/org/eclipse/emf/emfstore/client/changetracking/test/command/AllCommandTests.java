fb652e50015a4cc61442be5c8657c7261d03e748
Edgar Mueller <emueller@eclipsesource.com>
2015-06-19 10:36:54 +0200

    Bug 470559 - Provide test case for undo/redo of custom commands
    Renamed isUndo attribute to canUndo
    
    Change-Id: Ic6a444e3145db07cef32140759bfbc56b80596a4

--- tests/org.eclipse.emf.emfstore.client.changetracking.test/src/org/eclipse/emf/emfstore/client/changetracking/test/command/AllCommandTests.java
+++ tests/org.eclipse.emf.emfstore.client.changetracking.test/src/org/eclipse/emf/emfstore/client/changetracking/test/command/AllCommandTests.java
@@ -5,9 +5,9 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
- * koegel
+ * Maximilian Koegel - initial API and implementation
  ******************************************************************************/
 package org.eclipse.emf.emfstore.client.changetracking.test.command;
 
@@ -16,11 +16,14 @@ import org.junit.runners.Suite;
 
 /**
  * Test Suite for running all tests of canonization.
- * 
+ *
  * @author koegel
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ CommandTest.class })
+@Suite.SuiteClasses({
+	CommandTest.class,
+	UndoRedoCommandTest.class
+})
 public class AllCommandTests {
 
 }
---
+++ tests/org.eclipse.emf.emfstore.client.changetracking.test/src/org/eclipse/emf/emfstore/client/changetracking/test/command/UndoRedoCommandTest.java
@@ -0,0 +1,158 @@
+/*******************************************************************************
+ * Copyright (c) 2015 EclipseSource Muenchen GmbH and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Edgar Mueller - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.emf.emfstore.client.changetracking.test.command;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import org.eclipse.emf.common.command.AbstractCommand;
+import org.eclipse.emf.common.command.CommandStack;
+import org.eclipse.emf.emfstore.client.test.common.cases.ESTest;
+import org.eclipse.emf.emfstore.client.test.common.dsl.Create;
+import org.eclipse.emf.emfstore.client.test.common.util.ProjectUtil;
+import org.eclipse.emf.emfstore.client.util.RunESCommand;
+import org.eclipse.emf.emfstore.internal.client.model.ESWorkspaceProviderImpl;
+import org.eclipse.emf.emfstore.internal.client.model.impl.ProjectSpaceBase;
+import org.eclipse.emf.emfstore.internal.client.model.util.AbstractEMFStoreCommand;
+import org.eclipse.emf.emfstore.internal.client.observers.OperationObserver;
+import org.eclipse.emf.emfstore.internal.server.model.versioning.operations.AbstractOperation;
+import org.eclipse.emf.emfstore.test.model.TestElement;
+import org.junit.Test;
+
+/**
+ * Test that demonstrates
+ *
+ *
+ * @author emueller
+ *
+ */
+public class UndoRedoCommandTest extends ESTest {
+
+	public class MyCommand extends AbstractCommand {
+
+		private final ProjectSpaceBase projectSpace;
+		private final List<AbstractOperation> executedOperations;
+		private boolean canUndo;
+		private final Runnable runnable;
+
+		public MyCommand(ProjectSpaceBase projectSpace, Runnable runnable) {
+			this.projectSpace = projectSpace;
+			this.runnable = runnable;
+			executedOperations = new ArrayList<AbstractOperation>();
+		}
+
+		/**
+		 * {@inheritDoc}
+		 *
+		 * @see org.eclipse.emf.common.command.Command#execute()
+		 */
+		public void execute() {
+			final CommandStack commandStack =
+				ESWorkspaceProviderImpl.getInstance()
+					.getInternalWorkspace()
+					.getEditingDomain()
+					.getCommandStack();
+
+			// add operation observer during command execution to track executed operations
+			final OperationObserver operationObserver = createOperationObserver();
+			projectSpace.getOperationManager().addOperationObserver(operationObserver);
+			commandStack.execute(new AbstractEMFStoreCommand() {
+				@Override
+				protected void commandBody() {
+					runnable.run();
+				}
+			});
+			projectSpace.getOperationManager().removeOperationListener(operationObserver);
+
+			canUndo = true;
+		}
+
+		private OperationObserver createOperationObserver() {
+			final OperationObserver operationObserver = new OperationObserver() {
+
+				public void operationExecuted(AbstractOperation operation) {
+					executedOperations.add(operation);
+				}
+
+				public void operationUndone(AbstractOperation operation) {
+				}
+			};
+			return operationObserver;
+		}
+
+		public void redo() {
+
+			RunESCommand.run(new Callable<Void>() {
+				public Void call() throws Exception {
+					projectSpace.applyOperations(executedOperations, true);
+					return null;
+				}
+			});
+
+			canUndo = true;
+		}
+
+		@Override
+		public boolean canUndo() {
+			return canUndo;
+		}
+
+		@Override
+		public void undo() {
+			RunESCommand.run(new Callable<Void>() {
+				public Void call() throws Exception {
+					projectSpace.undoLastOperations(executedOperations.size());
+					return null;
+				}
+			});
+
+			canUndo = false;
+		}
+
+	}
+
+	@Test
+	public void undo() {
+
+		final TestElement leafSection = Create.testElement();
+
+		final MyCommand cmd = new MyCommand((ProjectSpaceBase) getProjectSpace(), new Runnable() {
+			public void run() {
+				ProjectUtil.addElement(getProjectSpace().toAPI(), leafSection);
+			}
+		});
+
+		cmd.execute();
+		cmd.undo();
+		assertEquals(0, getLocalProject().getModelElements().size());
+	}
+
+	@Test
+	public void redo() {
+
+		final TestElement leafSection = Create.testElement();
+
+		final MyCommand cmd = new MyCommand((ProjectSpaceBase) getProjectSpace(), new Runnable() {
+			public void run() {
+				ProjectUtil.addElement(getProjectSpace().toAPI(), leafSection);
+			}
+		});
+
+		cmd.execute();
+		cmd.undo();
+		cmd.redo();
+		assertEquals(1, getLocalProject().getModelElements().size());
+	}
+}
