0a55284d9949679848319529b28487ef086a1184
Edgar Mueller <emueller@eclipsesource.com>
2015-09-02 21:16:39 +0200

    Bug 467578 - ESLocalProject.getProjectName() causes ConcurrentModificationException
    Added test case
    
    Change-Id: Ia7ec18b66c83100fbfe53257d88cf6e189b51b4f

--- tests/org.eclipse.emf.emfstore.client.recording.test/src/org/eclipse/emf/emfstore/client/recording/test/OperationManagerTest.java
+++ tests/org.eclipse.emf.emfstore.client.recording.test/src/org/eclipse/emf/emfstore/client/recording/test/OperationManagerTest.java
@@ -13,15 +13,26 @@ package org.eclipse.emf.emfstore.client.recording.test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.util.ConcurrentModificationException;
+import java.util.List;
+
+import org.eclipse.emf.emfstore.client.ESLocalProject;
 import org.eclipse.emf.emfstore.client.test.common.cases.ESTest;
+import org.eclipse.emf.emfstore.client.test.common.dsl.Add;
 import org.eclipse.emf.emfstore.client.test.common.dsl.Create;
 import org.eclipse.emf.emfstore.client.util.ESVoidCallable;
 import org.eclipse.emf.emfstore.client.util.RunESCommand;
+import org.eclipse.emf.emfstore.common.ESObserver;
 import org.eclipse.emf.emfstore.internal.client.model.ESWorkspaceProviderImpl;
 import org.eclipse.emf.emfstore.internal.client.model.ProjectSpace;
+import org.eclipse.emf.emfstore.internal.client.model.util.EMFStoreCommand;
 import org.eclipse.emf.emfstore.internal.client.observers.OperationObserver;
+import org.eclipse.emf.emfstore.internal.common.observer.ObserverCall;
+import org.eclipse.emf.emfstore.internal.common.observer.ObserverCall.Result;
 import org.eclipse.emf.emfstore.internal.server.model.versioning.operations.AbstractOperation;
+import org.eclipse.emf.emfstore.test.model.TestElement;
 import org.junit.Test;
 
 /**
@@ -35,7 +46,11 @@ public class OperationManagerTest extends ESTest {
 	@Test
 	public void addObserver() {
 		final boolean[] operationExecuted = new boolean[] { false };
-		final OperationObserver observer = createOperationObserver(operationExecuted);
+		final OperationObserver observer = createOperationObserver(new Runnable() {
+			public void run() {
+				operationExecuted[0] = true;
+			}
+		});
 		ESWorkspaceProviderImpl.getObserverBus().register(observer);
 		RunESCommand.run(new ESVoidCallable() {
 			@Override
@@ -50,24 +65,63 @@ public class OperationManagerTest extends ESTest {
 	@Test
 	public void removeObserver() {
 		final boolean[] operationExecuted = new boolean[] { false };
-		final OperationObserver operationObserver = createOperationObserver(operationExecuted);
-		ESWorkspaceProviderImpl.getObserverBus().register(operationObserver);
-		ESWorkspaceProviderImpl.getObserverBus().unregister(operationObserver);
-		RunESCommand.run(new ESVoidCallable() {
-			@Override
+		final OperationObserver observer = createOperationObserver(new Runnable() {
 			public void run() {
-				getLocalProject().getModelElements().add(Create.testElement());
+				operationExecuted[0] = true;
 			}
 		});
+		ESWorkspaceProviderImpl.getObserverBus().register(observer);
+		ESWorkspaceProviderImpl.getObserverBus().unregister(observer);
+		Add.toProject(getLocalProject(), Create.testElement());
 		assertFalse(operationExecuted[0]);
 	}
 
-	private static OperationObserver createOperationObserver(final boolean[] operationExecutedFlag) {
+	// @Ignore
+	@Test
+	public void bug467578() {
+		final OperationObserver observer = createOperationObserver(new Runnable() {
+			public void run() {
+				try {
+					final A proxy = ESWorkspaceProviderImpl.getObserverBus().notify(A.class);
+					proxy.foo(getLocalProject());
+					final List<Result> results = ((ObserverCall) proxy).getObserverCallResults();
+					if (results.get(0).exceptionOccurred()) {
+						fail(results.get(0).getException().getMessage());
+					}
+				} catch (final ConcurrentModificationException ex) {
+					fail(ex.getMessage());
+				}
+			}
+		});
+		final AImpl a = new AImpl();
+		ESWorkspaceProviderImpl.getObserverBus().register(observer);
+		ESWorkspaceProviderImpl.getObserverBus().register(a);
+		Add.toProject(getLocalProject(), Create.testElement());
+		ESWorkspaceProviderImpl.getObserverBus().unregister(observer);
+		ESWorkspaceProviderImpl.getObserverBus().unregister(a);
+	}
+
+	public interface A extends ESObserver {
+		void foo(ESLocalProject localProject);
+	}
+
+	public class AImpl implements A {
+		public void foo(ESLocalProject localProject) {
+			final TestElement testElement = (TestElement) localProject.getModelElements().get(0);
+			new EMFStoreCommand() {
+				@Override
+				protected void doRun() {
+					testElement.setName("foo"); //$NON-NLS-1$
+				}
+			}.run(false);
+		}
+	}
+
+	private static OperationObserver createOperationObserver(final Runnable runnable) {
 		return new OperationObserver() {
 
 			public void operationExecuted(ProjectSpace projectSpace, AbstractOperation operation) {
-				operationExecutedFlag[0] = true;
-
+				runnable.run();
 			}
 
 			public void operationUndone(ProjectSpace projectSpace, AbstractOperation operation) {
@@ -75,4 +129,5 @@ public class OperationManagerTest extends ESTest {
 			}
 		};
 	}
+
 }
