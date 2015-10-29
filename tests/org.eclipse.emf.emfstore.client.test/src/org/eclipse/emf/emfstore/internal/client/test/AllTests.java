c4e74786cbff90662465e60c10eb0ecd1d4be530
Edgar Mueller <emueller@eclipsesource.com>
2015-10-29 18:01:01 +0100

    Bug 480454 - Corrupted ChangePackage resources in case of active change package splitting
    Added test case. Also added second test case that checks whether the
    project space is saved correctly
    
    Change-Id: I58838d7c0e4a708f184d1512dd3e9b6a44b0d949

--- tests/org.eclipse.emf.emfstore.client.test/src/org/eclipse/emf/emfstore/internal/client/test/AllTests.java
+++ tests/org.eclipse.emf.emfstore.client.test/src/org/eclipse/emf/emfstore/internal/client/test/AllTests.java
@@ -5,7 +5,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * koegel
  ******************************************************************************/
@@ -14,19 +14,22 @@ package org.eclipse.emf.emfstore.internal.client.test;
 import org.eclipse.emf.emfstore.internal.client.test.caching.AllCachingTests;
 import org.eclipse.emf.emfstore.internal.client.test.common.AllCommonTests;
 import org.eclipse.emf.emfstore.internal.client.test.persistence.AllPersistenceTests;
+import org.eclipse.emf.emfstore.internal.client.test.workspace.AllWorkspaceTests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 /**
  * Runs all tests.
- * 
+ *
  * @author koegel
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 	AllCachingTests.class,
 	AllCommonTests.class,
-	AllPersistenceTests.class })
+	AllPersistenceTests.class,
+	AllWorkspaceTests.class
+})
 public class AllTests {
 
 }
